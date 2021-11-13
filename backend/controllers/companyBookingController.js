d766a4d5be266fa004822123a805f4b09041bb9a
Rizal M <mrizalmaulana000@yahoo.com>
2021-11-13 23:25:42 +0700

    add booking controller for company user

--- backend/controllers/companyBookingController.js
+++ backend/controllers/companyBookingController.js
@@ -1,39 +1,18 @@
 const asyncHandler = require('express-async-handler');
-const CompanyUser = require('../models/companyUser');
-const jwt = require('jsonwebtoken');
+const Booking = require('../models/booking');
 
-const login = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+const getBooking = asyncHandler(async (req, res) => {
+    const bookings =  await Booking.find().populate('event').populate('vendor').populate('company');
 
-    const user = await CompanyUser.findOne({ email });
-
-    if (!user) {
-        return res.status(400).json({ message: 'Invalid Credentials' });
-    }
-
-    const isMatch = password === user.password;
-    if (!isMatch) {
-        return res.status(400).json({ message: 'Invalid Credentials' });
-    }
-
-    const payload = {
-        user: { 
-            id: user._id
-        }
-    }
-
-    jwt.sign(payload, 'secret', {
-        expiresIn: 3600
-    }, (err, token) => {
-        if(err) throw err;
-        res.status(200).json({
-            _id: user._id,
-            company_name: user.company_name,
-            email: user.email,   
-            token : token
-        });
-    });
-
+    res.json(bookings);
 });
 
-module.exports = { login }; 
+const storeBooking = asyncHandler(async (req, res) => {
+    const { event, vendor, company, location, proposed_date_1, proposed_date_2, proposed_date_3 } = req.body;
+    
+    const booking = new Booking({event, vendor, company, location, proposed_date_1, proposed_date_2, proposed_date_3});
+    booking.save();
+    res.status(201).json({ status: true, message: 'Booking created' });        
+});
+
+module.exports = { getBooking, storeBooking }; 
