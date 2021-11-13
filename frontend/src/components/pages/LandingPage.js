9a05885928f5eefaf8783d0e8cea2275a5e9e06d
Rizal M <mrizalmaulana000@yahoo.com>
2021-11-13 19:33:57 +0700

    update landing page

--- frontend/src/components/pages/LandingPage.js
+++ frontend/src/components/pages/LandingPage.js
@@ -1,12 +1,23 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
     return(
         <Fragment>
-            <h2>Welcome</h2>
-            <p>This is just a test app</p>
+            <div className='mt-5 p-5 bg-white rounded shadow-lg'>
+                <h2 className='uppercase text-3xl font-bold'>Welcome</h2>
 
-            <h2>How to use :</h2>
+                <div className='mt-3'>
+                    <p>This is just a test app</p>
+                    <h2 className='font-bold text-xl'># How to use :</h2>
+                    <p>Login as HR Company User, go to <Link to='/login' className='bg-blue-500 py-1 px-4 text-white rounded hover:bg-blue-400'> Login Page </Link></p>
+                
+                    <h3 className='font-bold mt-2 text-xl'> Company User </h3>
+                    <p> Email : firsthr@firstcompany.com </p>
+                    <p> Password : 123456 </p>
+
+                </div>         
+            </div>            
         </Fragment>
     )
 };
