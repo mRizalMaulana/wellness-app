9ba49f6ab6f7c78b0d2d2af717a60d8dac9512fa
Rizal M <mrizalmaulana000@yahoo.com>
2021-11-14 00:15:06 +0700

    create event action

---
+++ frontend/src/actions/event.js
@@ -0,0 +1,33 @@
+import { 
+    EVENT_LIST_REQUEST,
+    EVENT_LIST_SUCCESS,
+    EVENT_LIST_FAIL
+} from "../constants/vendor"
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const EventList = () => async (dispatch, getState) => {
+    try {
+        dispatch({ type: EVENT_LIST_REQUEST });
+
+        const { userLogin: { companyUser } } = getState();
+        const baseUrl = process.env.REACT_APP_BACKEND_HOST;
+        const config = { headers: { 
+            "Authorization" : `Bearer ${companyUser.token}`,
+            "Content-type"  : "application/json" 
+        } };
+        
+        const { data } = await axios.get(`${baseUrl}/api/event`, config);
+ 
+        dispatch({ type: EVENT_LIST_SUCCESS, payload:data });
+    } catch (err) {
+        dispatch({
+            type: EVENT_LIST_FAIL,
+            payload: 
+                err.response && err.response.data.message 
+                    ? err.response.data.message : err.message
+        });
+    }
+}
