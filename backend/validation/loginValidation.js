fb271a86fa38e5bf014a855a9891c8c49ae2a9ea
Rizal M <mrizalmaulana000@yahoo.com>
2021-11-13 14:25:16 +0700

    change login validation condition

--- backend/validation/loginValidation.js
+++ backend/validation/loginValidation.js
@@ -4,9 +4,11 @@ exports.validateLogin = [
     check('email', 'Please add a valid email').isEmail(),
     check('password', 'Please add password').not().isEmpty(),
     (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty())
-        return res.status(400).json({errors: errors.array()});
-      next();
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({message: errors.array()[0].msg})
+        }
+
+        next();
     },
   ];
